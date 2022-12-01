6d0fbe8a2c4d19fa6fe7d1bbf1aa17c8c12bdd7f
澤田悠太 <yuta2929ayax@gmail.com>
2022-12-01 19:19:21 +0900

    tett

---
+++ functions/index.js
@@ -0,0 +1,74 @@
+import { initializeApp } from "firebase-admin";
+import { initializeApp } from "firebase-admin/app";
+const functions = require("firebase-functions");
+const gcs = require("@google-cloud/storage")();
+const spawn = require("child-process-promise").spawn;
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+var gs = require("gs");
+
+exports.makePNG = functions.storage.object().onChange((event) => {
+	// ignore delete events
+	if (event.data.resourceState == "not_exists") return false;
+
+	const filePath = event.data.name;
+	const fileDir = path.dirname(filePath);
+	const fileName = path.basename(filePath);
+	const tempFilePath = path.join(os.tmpdir(), fileName);
+	if (fileName.endsWith(".png")) return false;
+	if (!fileName.endsWith(".pdf")) return false;
+
+	const newName = path.basename(filePath, ".pdf") + ".png";
+	const tempNewPath = path.join(os.tmpdir(), newName);
+
+	// // Download file from bucket.
+	const bucket = gcs.bucket(event.data.bucket);
+
+	return bucket
+		.file(filePath)
+		.download({
+			destination: tempFilePath,
+		})
+		.then(() => {
+			console.log("Image downloaded locally to", tempFilePath);
+
+			return new Promise(function (resolve, reject) {
+				gs()
+					.batch()
+					.nopause()
+					.option("-r" + 50 * 2)
+					.option("-dDownScaleFactor=2")
+					.executablePath("lambda-ghostscript/bin/./gs")
+					.device("png16m")
+					.output(tempNewPath)
+					.input(tempFilePath)
+					.exec(function (err, stdout, stderr) {
+						if (!err) {
+							console.log("gs executed w/o error");
+							console.log("stdout", stdout);
+							console.log("stderr", stderr);
+							resolve();
+						} else {
+							console.log("gs error:", err);
+							reject(err);
+						}
+					});
+			});
+		})
+		.then(() => {
+			console.log("PNG created at", tempNewPath);
+
+			// Uploading the thumbnail.
+			return bucket.upload(tempNewPath, { destination: newName });
+			// Once the thumbnail has been uploaded delete the local file to free up disk space.
+		})
+		.then(() => {
+			fs.unlinkSync(tempNewPath);
+			fs.unlinkSync(tempFilePath);
+		})
+		.catch((err) => {
+			console.log("exception:", err);
+			return err;
+		});
+});
