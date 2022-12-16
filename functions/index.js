7d35ea34d5ab3e077ec4cdf14d67948236bedc73
澤田悠太 <yuta2929ayax@gmail.com>
2022-12-16 21:19:15 +0900

    できたー

--- functions/index.js
+++ functions/index.js
@@ -1,86 +1,3 @@
-// const functions = require("firebase-functions");
-// // const gcs = require("@google-cloud/storage");
-// const spawn = require("child-process-promise").spawn;
-// const path = require("path");
-// const os = require("os");
-// const fs = require("fs");
-// var gs = require("gs");
-
-// // const functions = require("firebase-functions");
-// const gcs = require("@google-cloud/storage")();
-// // const spawn = require("child-process-promise").spawn;
-// // const path = require("path");
-// // const os = require("os");
-// // const fs = require("fs");
-// // var gs = require("gs");
-
-// exports.makePNG = functions.storage.object().onFinalize((event) => {
-// 	// ignore delete events
-// 	if (event.data.resourceState == "not_exists") return false;
-
-// 	const filePath = event.data.name;
-// 	// const fileDir = path.dirname(filePath);
-// 	const fileName = path.basename(filePath);
-// 	const tempFilePath = path.join(os.tmpdir(), fileName);
-// 	if (fileName.endsWith(".png")) return false;
-// 	if (!fileName.endsWith(".pdf")) return false;
-
-// 	const newName = path.basename(filePath, ".pdf") + ".png";
-// 	const tempNewPath = path.join(os.tmpdir(), newName);
-
-// 	// // Download file from bucket.
-// 	const bucket = gcs.bucket(event.data.bucket);
-// 	// const bucket = admin.storage().bucket(fileBucket);
-
-// 	return bucket
-// 		.file(filePath)
-// 		.download({
-// 			destination: tempFilePath,
-// 		})
-// 		.then(() => {
-// 			console.log("Image downloaded locally to", tempFilePath);
-
-// 			return new Promise(function (resolve, reject) {
-// 				gs()
-// 					.batch()
-// 					.nopause()
-// 					.option("-r" + 50 * 2)
-// 					.option("-dDownScaleFactor=2")
-// 					.executablePath("lambda-ghostscript/bin/./gs")
-// 					.device("png16m")
-// 					.output(tempNewPath)
-// 					.input(tempFilePath)
-// 					.exec(function (err, stdout, stderr) {
-// 						if (!err) {
-// 							console.log("gs executed w/o error");
-// 							console.log("stdout", stdout);
-// 							console.log("stderr", stderr);
-// 							resolve();
-// 						} else {
-// 							console.log("gs error:", err);
-// 							reject(err);
-// 						}
-// 					});
-// 			});
-// 		})
-// 		.then(() => {
-// 			console.log("PNG created at", tempNewPath);
-
-// 			// Uploading the thumbnail.
-// 			return bucket.upload(tempNewPath, { destination: newName });
-// 			// Once the thumbnail has been uploaded delete the local file to free up disk space.
-// 		})
-//         .then(() => {
-//             // 一時的に作ったファイルを消す
-// 			fs.unlinkSync(tempNewPath);
-// 			fs.unlinkSync(tempFilePath);
-// 		})
-// 		.catch((err) => {
-// 			console.log("exception:", err);
-// 			return err;
-// 		});
-// });
-
 "use strict";
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
@@ -96,29 +13,27 @@ exports.generateThumbnail = functions.storage.object().onFinalize(async (object)
 	if (object.contentType !== "application/pdf") {
 		return;
 	}
-	// const contentType = object.contentType; // File content type.
-	// Get the file name.
+
 	const fileBucket = object.bucket; // The Storage bucket that contains the file.
 	const filePath = object.name; // File path in the bucket.
 	const fileName = path.basename(filePath);
 
-	// Exit if the image is already a thumbnail.
 	// if (fileName.startsWith("thumb_")) {
 	// 	return functions.logger.log("Already a Thumbnail.");
 	// }
-	// Download file from bucket.
 	const bucket = admin.storage().bucket(fileBucket);
 	const tempFilePath = path.join(os.tmpdir(), fileName);
-	const outputFile = tempFilePath.replace(".pdf", ".jpeg");
+	// const outputFile = tempFilePath.replace(".pdf", ".jpeg");
+	const outputFile = path.join(os.tmpdir(), "output.jpeg");
+
+	const metadata = {
+		contentType: "application/pdf",
+	};
 
 	console.log("filepath is ", filePath);
 	console.log("filename is ", fileName);
 	console.log("tmpfilepath is ", tempFilePath);
 	console.log("outputfile is ", outputFile);
-
-	// const metadata = {
-	// 	contentType: contentType,
-	// };
 
 	await bucket.file(filePath).download({ destination: tempFilePath });
 
@@ -136,6 +51,7 @@ exports.generateThumbnail = functions.storage.object().onFinalize(async (object)
 				console.log("gs executed w/o error");
 				console.log("stdout", stdout);
 				console.log("stderr", stderr);
+				bucket.upload(outputFile, { destination: outputFile });
 				resolve();
 			} else {
 				console.log("gs error:", err);
@@ -143,14 +59,19 @@ exports.generateThumbnail = functions.storage.object().onFinalize(async (object)
 			}
 		});
 
-	// const thumbFileName = `thumb_${outputFile}`;
-	// const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
-	// Uploading the thumbnail.
-	await bucket.upload(outputFile, {
-		destination: outputFile,
-		// metadata: metadata,
-	});
-	return fs.unlinkSync(tempFilePath);
+	// // const thumbFileName = `thumb_${outputFile}`;
+	// const thumbFilePath = path.join(path.dirname(filePath), outputFile);
+
+	// // Uploading the thumbnail.
+	// await bucket.upload(outputFile, {
+	// 	destination: outputFile,
+	// 	metadata: metadata,
+	// });
+
+	// console.log("output file is second ", outputFile);
+	// console.log("thumbfilepath", thumbFilePath);
+	// return fs.unlinkSync(tempFilePath);
+	return;
 });
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
